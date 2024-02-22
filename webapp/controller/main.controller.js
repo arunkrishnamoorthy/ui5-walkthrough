dc84ff8acc73ec30865b48e38c8b7694f9177810
Arun Krishnamoorthy <arunmba.rec@gmail.com>
2024-02-22 15:10:48 +0100

    Model

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -1,11 +1,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function(Controller,MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function(Controller,MessageToast,JSONModel) {
 
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.main", {
+
+        onInit: function() {
+            let data = {
+                name: "Hari"
+            };
+            let model = new JSONModel(data);
+            this.getView().setModel(model);
+        },
 
         onPressDisplay: function() {
             MessageToast.show("This is a message from message toast");
