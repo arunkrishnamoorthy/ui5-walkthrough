be88b2aefa5bd444c61a19f996ff3701ba8963b8
Arun Krishnamoorthy <arunmba.rec@gmail.com>
2024-02-22 15:31:37 +0100

    Translatable texts

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function(Controller,MessageToast,JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function(Controller,MessageToast,JSONModel,ResourceModel) {
 
     "use strict";
 
@@ -14,6 +15,11 @@ sap.ui.define([
             };
             let model = new JSONModel(data);
             this.getView().setModel(model);
+
+            let i18nModel = new ResourceModel({
+                bundleName: "ui5.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
 
         onPressDisplay: function() {
